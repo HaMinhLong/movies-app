9f8517a8f0725c029ee83369de3393d8dd43238b
Minh Long <haminhlong3@gmail.com>
2020-12-28 23:47:24 +0000

    mongodb setup and connect to the database

--- server/server.js
+++ server/server.js
@@ -4,11 +4,26 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 5000;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port port!`));
+const CONNECTION_URL =
+  "mongodb+srv://nar_89:Na+89-K-2@cluster0.bystm.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+mongoose.set("useFindAndModify", false);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
