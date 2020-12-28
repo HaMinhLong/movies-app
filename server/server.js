81c4db0456c606ed8084cf63f098ddbd58e6af1a
Minh Long <haminhlong3@gmail.com>
2020-12-28 23:22:44 +0000

    server.js setup

--- server/server.js
+++ server/server.js
@@ -6,5 +6,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port port!`));
