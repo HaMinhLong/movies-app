d41a8c228bf74e75faa712c3f28a8c1b10ead980
Minh Long <haminhlong3@gmail.com>
2020-12-29 08:36:45 +0000

    add controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,26 @@
-const getPosts = (req, res) => {
-  res.send("This Work!");
+const postMessage = require("../models/postMessage");
+
+const getPosts = async (req, res) => {
+  try {
+    const postMessage = await postMessage.find();
+    console.log(postMessage);
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new postMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 module.exports = getPosts;
+module.exports = createPost;
