bef12a48cb268d2e6fb4ffffcf625b890cda5fae
Minh Long <haminhlong3@gmail.com>
2020-12-29 20:17:51 +0000

    add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Banner from "./components/Layout/Banner";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import MoreDetails from "./components/MoreDetails/MoreDetails";
 import Footer from "./components/Layout/Footer";
 
+import { getPosts } from "./actions/posts";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Router>
       <Route path="/" exact component={Banner} />
