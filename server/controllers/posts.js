26cfe96cbfe22045fde05030b158bf92356ec3f7
Minh Long <haminhlong3@gmail.com>
2020-12-29 08:13:21 +0000

    add backend folder structure

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+const getPosts = (req, res) => {
+  res.send("This Work!");
+};
+
+module.exports = getPosts;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const getPosts = require("../controllers/posts");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This Work!");
-});
+router.get("/", getPosts);
 
 module.exports = router;
